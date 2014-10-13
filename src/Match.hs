2e92481beeb457a5b76221a98e5ba58a36a819b6
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-10-13 23:55:19 +0900

    work

--- src/Match.hs
+++ src/Match.hs
@@ -1,42 +1,57 @@
 import Data.Complex
-import Data.Direction
 import Control.Applicative
+import qualified Data.Foldable as F
 import qualified Data.Set as Set
+import Linear
+import Codec.Picture
+import System.Random
 
-type RealPiece
+type RealPiece = Piece [Piece (V2 Int)]
 
-fromImage :: C.Image PixelRGBA8 -> [RealPiece]
-fromImage = undefined
+fromImage :: Image PixelRGB8 -> Int -> Int -> [RealPiece]
+fromImage img c r = [Piece 1
+  (squash [pixelAt img x (h * r) | x <- [w*c..w*succ c-1]])
+  (squash [pixelAt img x (h * succ r - 1) | x <- [w*c..w*succ c-1]])
+  0
+  [Piece
+    1
+    (squash [pixelAt img (w * c) y | y <- [h*r..h*succ r-1]])
+    (squash [pixelAt img (w * succ c - 1) y | y <- [h*r..h*succ r-1]])
+    0
+    (V2 c r)]
+  | c <- [0..c-1], r <- [0..r-1]] where
+  w = imageWidth img `div` c
+  h = imageHeight img `div` r
 
-type Chara = [Complex]
+squash :: [PixelRGB8] -> Chara
+squash ps = V4 (gen 0.1 0 xs) (gen 0.1 0 ys) (gen 0.1 0 zs) (gen 0.2 0 $ zipWith (+) xs ys) where
+  (xs, ys, zs) = foldr 
+    (\(PixelRGB8 r g b) (rs, gs, bs) -> (fromIntegral r : rs, fromIntegral g : gs, fromIntegral b : bs))
+    ([], [], [])
+    ps
+
+shrink :: IM.IntMap (Piece a) -> IM.IntMap (Piece a)
+shrink n m = id
+
+combine (Piece m p q x a) (Piece n r s y b) = Piece (m + n) p s (distanceC q r) (a ++ b)
+
+type Chara = V4 (Complex Double)
 
 distanceC :: Chara -> Chara -> Double
-distanceC a b = sum $ map (^2) $ zipWith subtract a b
+distanceC a b = F.sum $ fmap g2 $ liftA2 subtract a b where
+  g2 :: Complex Double -> Double
+  g2 (a :+ b) = a^2 + b^2
 
-gen :: Freq -> Phase -> [Double] -> Complex Double
+gen :: Double -> Double -> [Double] -> Complex Double
 gen _ _ [] = 0
-gen f p (x:xs) = cis p * x + gen f (p + f) xs 
+gen f p (x:xs) = cis p * (x :+ 0) + gen f (p + f) xs 
 
 data Piece a = Piece
     Int
     Chara -- top
     Chara -- bottom
-    Double -- Local divergence
+    { divergence :: Double } -- Local divergence
     a
-
-instance Monoid a => Monoid (Piece a) where
-    mempty = Piece 0 [] [] 0 mempty
-    mappend (Piece m p q x a) (Piece n r s y b) = Piece (m + n) p s (distanceC q r) (mappend a b)
-
-data Space a = Tip a | Space (V4 (V4 (Space a)))
-
-release :: [Piece a] -> Space (Piece a)
-
-popNearest :: Chara -> Space (Piece a) -> Maybe (Piece a, Space (Piece a)) -- O(logN) if possible
-
-merge :: Eq a => Space (Piece a) -> [Piece a] -> [Piece a]
-merge sp (x:xs) = undefined
-merge sp [] = []
 
 -- 0.
 -- 1. calculate the characteristics for every edge
